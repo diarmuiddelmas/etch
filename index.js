e17ff5f8eead2520f1459c81725a95275dd6c419
Diarmuid Delmas <230876504+diarmuiddelmas@users.noreply.github.com>
2025-10-18 23:18:49 +0200

    Etch a Sketch: Add button prompting user to set the number of squares per side

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
 
+let squares = [];
 let rows = 16;
-let columns = 16;
 
-      // Use a nested for loop to create the grid squares
+document.documentElement.style.setProperty("--rows", rows);
+
+createGrid(rows);
+
+    //   for (let i = 0; i < rows; i++) {
+        // for (let j = 0; j < rows; j++) {
+          
+        //   const square = document.createElement("div");
+        //    square.classList.add("square");
+        //    container.appendChild(square);
+        //    squares.push(square);
+
+        //   square.addEventListener("mouseover", () => {
+        //   square.style.backgroundColor = "red";
+
+// })
+// }
+// }
+
+button.addEventListener("click", () => {
+
+let input = parseInt(prompt("Enter the number of squares per side"),);
+rows = input;
+document.documentElement.style.setProperty("--rows", rows);
+
+container.replaceChildren();
+squares = [];
+createGrid(rows);
+});
+
+function createGrid (rows) {
+    
       for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < columns; j++) {
-          const square = document.createElement('div');
-          square.classList.add('square');
-          container.appendChild(square);
+        for (let j = 0; j < rows; j++) {
+          
+          const square = document.createElement("div");
+           square.classList.add("square");
+           
+           container.appendChild(square);
+           squares.push(square);
 
           square.addEventListener("mouseover", () => {
           square.style.backgroundColor = "red";
-})
-}
-}
+          });
 
+        }}}
